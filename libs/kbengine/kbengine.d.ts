fd1435cf6bc08d300a7eb786e56ffaca03b0a840
joshuya_wu <joshuya_wu@163.com>
2017-06-18 23:44:48 +0800

    add kbengine

---
+++ libs/kbengine/kbengine.d.ts
@@ -0,0 +1,8 @@
+declare module KBEngine {
+    function getInstance();
+    function KBEngineArgs();
+    function create(kbengineArgs);
+    function destroy();
+    function Event();
+    var GameObject:any;
+}
